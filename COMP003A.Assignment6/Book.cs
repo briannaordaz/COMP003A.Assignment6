281e9e873378a19ac268a37b0ba401ec58bcaae4
briannaordaz <briannaordaz5@gmail.com>
2026-02-20 20:46:05 -0800

    completed required class specifications, steps 1, 2, and 3

---
+++ COMP003A.Assignment6/Book.cs
@@ -0,0 +1,28 @@
+namespace COMP003A.Assignment6;
+
+public class Book
+{
+    public string title;
+    public int bookId;
+    public bool checkedOut;
+
+    public Book(string title, int bookId, bool checkedOut)
+    {
+        this.title = title;
+        this.bookId = bookId;
+        this.checkedOut = checkedOut;
+    }
+
+    public void DisplayAllInfo()
+    {
+        Console.WriteLine($"Book Title: {title}\nBook Id: {bookId}\nCheckedOut: {checkedOut}");
+    }
+
+    public void ToggleOut()
+    {
+        checkedOut = !checkedOut;
+        
+    }
+    
+    
+}
